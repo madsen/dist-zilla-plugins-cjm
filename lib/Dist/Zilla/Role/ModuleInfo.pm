46d2da63c878f0150bcf125efc54882adc5dcf1c
Christopher J. Madsen <perl@cjmweb.net>
2009-10-05 22:51:05 -0500

    Change get_module_info to use a temporary directory   Module::Build::ModuleInfo expects the filename to match the package name Added documentation

--- lib/Dist/Zilla/Role/ModuleInfo.pm
+++ lib/Dist/Zilla/Role/ModuleInfo.pm
@@ -14,18 +14,33 @@ package Dist::Zilla::Role::ModuleInfo;
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
 # GNU General Public License or the Artistic License for more details.
 #
-# Create Module::Build::ModuleInfo object from Dist::Zilla::Role::File
+# ABSTRACT: Create Module::Build::ModuleInfo object from Dist::Zilla::File
 #---------------------------------------------------------------------
 
 our $VERSION = '0.01';
 
 use Moose::Role;
-use Moose::Autobox;
 
+use autodie ':io';
 use File::Temp ();
 use Module::Build::ModuleInfo ();
+use Path::Class qw(dir file);
 
-#---------------------------------------------------------------------
+=head1 DESCRIPTION
+
+Plugins implementing ModuleInfo may call their own C<get_module_info>
+method to construct a L<Module::Build::ModuleInfo> object.
+
+=method get_module_info
+
+  my $info = $plugin->get_module_info($file);
+
+This constructs a Module::Build::ModuleInfo object from the contents
+of a C<$file> object that does Dist::Zilla::Role::File.  Any additional
+arguments are passed along to C<< Module::Build::ModuleInfo->new_from_file >>.
+
+=cut
+
 sub get_module_info
 {
   my $self = shift;
@@ -35,11 +50,18 @@ sub get_module_info
   # Module::Build::ModuleInfo doesn't have a new_from_string method,
   # so we'll write the current contents to a temporary file:
 
-  my $temp = File::Temp->new();
+  my $tempdirObject = File::Temp->newdir();
+  my $dir     = dir("$tempdirObject");
+  my $modPath = file($file->name);
 
+  # Module::Build::ModuleInfo only cares about the basename of the file:
+  my $tempname = $dir->file($modPath->basename);
+
+  open(my $temp, '>', $tempname);
   print $temp $file->content;
+  close $temp;
 
-  return Module::Build::ModuleInfo->new_from_file("$temp", @_)
+  return Module::Build::ModuleInfo->new_from_file("$tempname", @_)
       or die "Unable to get module info from " . $file->name . "\n";
 } # end get_module_info
 

2291704d8f0dbd018603106ef2e3148d149fd679
Christopher J. Madsen <perl@cjmweb.net>
2009-10-07 20:59:15 -0500

    PodLoom can now read data from file   Added documentation

--- lib/Dist/Zilla/Plugin/PodLoom.pm
+++ lib/Dist/Zilla/Plugin/PodLoom.pm
@@ -34,10 +34,68 @@ with qw(Dist::Zilla::Role::FileMunger
 use Hash::Merge::Simple ();
 use Pod::Loom ();
 
+=attr template
+
+This will be passed to Pod::Loom as its C<template>.
+Defaults to C<Default>.
+
+=cut
+
 has template => (
   is      => 'ro',
   isa     => 'Str',
   default => 'Default',
+);
+
+=attr data
+
+Since Pod::Loom templates may want configuration that doesn't fit in
+an INI file, you can specify a file containing Perl code to evaluate.
+The result should be a hash reference, which will be passed to
+Pod::Loom's C<weave> method.
+
+PodLoom automatically includes the following keys, which will be
+merged with the hashref from your code.  (Your code can override these
+values.)
+
+=over
+
+=item abstract
+
+The abstract for the file being processed
+
+=item authors
+
+C<< $zilla->authors >>
+
+=item dist
+
+C<< $zilla->name >>
+
+=item license_notice
+
+C<< $zilla->license->notice >>
+
+=item module
+
+The primary package of the file being processed
+
+=item version
+
+The version number of the file being processed
+
+=item zilla
+
+The Dist::Zilla object itself
+
+=back
+
+=cut
+
+has data_file => (
+  is       => 'ro',
+  isa      => 'Str',
+  init_arg => 'data',
 );
 
 has data => (
@@ -56,11 +114,28 @@ has loom => (
   default  => sub { Pod::Loom->new(template => shift->template) },
 );
 
+#---------------------------------------------------------------------
 sub _initialize_data
 {
-  return {};                    # FIXME read from file
+  my $plugin = shift;
+
+  my $fname = $plugin->data_file;
+
+  return {} unless $fname;
+
+  open my $fh, '<', $fname or die "can't open $fname for reading: $!";
+  my $code = do { local $/; <$fh> };
+  close $fh;
+
+  local $@;
+  my $result = eval "package Dist::Zilla::Plugin::PodLoom::_eval; $code";
+
+  die $@ if $@;
+
+  return $result;
 } # end _initialize_data
 
+#---------------------------------------------------------------------
 sub munge_file
 {
   my ($self, $file) = @_;
@@ -87,7 +162,7 @@ sub munge_file
   $file->content( $self->loom->weave(\$content, $file->name, $dataHash) );
 
   return;
-}
+} # end munge_file
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

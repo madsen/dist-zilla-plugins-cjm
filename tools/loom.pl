14f969e601fbc9831cb4c34520eff2d9edcc4cba
Christopher J. Madsen <perl@cjmweb.net>
2009-10-11 18:42:24 -0500

    Configure Pod::Loom to sort entries

--- tools/loom.pl
+++ tools/loom.pl
@@ -6,6 +6,9 @@ use strict;
 use warnings;
 
 {
+  sort_attr   => 1,
+  sort_diag   => 1,
+  sort_method => 1,
   # This template will be filled in by TemplateCJM:
   version_desc => <<'END VERSION',
 This document describes version {{$version}} of

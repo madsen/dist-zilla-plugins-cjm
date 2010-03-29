e77c0ce5aeb6470cfe2ce98a7903623fd502df78
Christopher J. Madsen <perl@cjmweb.net>
2010-03-29 14:23:01 -0500

    Added TestRelease
      Incremented version of PluginBundle::CJM to 0.05

--- lib/Dist/Zilla/PluginBundle/CJM.pm
+++ lib/Dist/Zilla/PluginBundle/CJM.pm
@@ -17,7 +17,7 @@ package Dist::Zilla::PluginBundle::CJM;
 # ABSTRACT: Build a distribution like CJM
 #---------------------------------------------------------------------
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 # This file is part of {{$dist}} {{$dist_version}} ({{$date}})
 
 use Moose;
